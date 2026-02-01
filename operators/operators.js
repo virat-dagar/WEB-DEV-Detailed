3091d39da2fda82988f21b9a4ef30f9b721b0219
Krishang <krishangmahajan9911@gmail.com>
2026-02-01 23:58:26 +0530

    Added operators

---
+++ operators/operators.js
@@ -0,0 +1,28 @@
+// Operators: Symbols that are used to perform operations on values (or operands)
+
+// 1) Arithmetic operators
+//   Used for performing standard mathematical calculations
+
+//   Addition(+)   Subtraction(-)   Multiplication(*)
+//   Division(/)   Modulo(%)        Exponentiation(**)
+
+// 2) Assignment operators
+//   Used to assign values to variables
+//   Add and assign(+=)   Subtract and assign(-=)   Multiply and assign(*=)
+//   Divide and assign(/=)
+
+// 3) Comparison operators
+//   Used to compare two values 
+//   Return true or false boolean value based on the result of the comparison
+
+// >(Greater than)  <(Less than)   >= (greater than or equal to)
+// <= (less than or equal to)      == (Equal to)
+// != (Not equal to)               !== (Strictly not equal to)
+// === (Strictly equal to)         < (Less than)
+
+// 4) Logical operators
+//  a) Logical AND (&&)  b) Logical OR (||)  c) Logical NOT (!)
+
+// 5) Ternary operator
+//  Shortcut for if-else
+    
