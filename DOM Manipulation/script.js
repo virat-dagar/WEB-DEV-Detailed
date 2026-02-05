93ec1d777c0ea5a37e5728cb457c44c500d95c68
Krishang <krishangmahajan9911@gmail.com>
2026-02-05 11:10:40 +0530

    Added DOM Manipulation

---
+++ DOM Manipulation/script.js
@@ -0,0 +1,59 @@
+// 
+
+const para = document.getElementsByTagName("p");
+console.log(para);
+// para.style.color = "blue";  wrong
+// for (let i=0; i<para.length; i++){
+//     if (i % 2 !== 0 && i<=para.length){
+//         para[i].style.color = "blue";
+//     }
+//     else {
+//         para[i].style.color = "green"
+//     }
+// }
+// para[para.length-1].style.fontWeight = "bold";
+
+// for (let i=0; i<para.length; i++){
+//     if (i % 2 !== 0 && i<=para.length){
+//         para[i].style.color = "blue";
+//     }
+//     else if (i % 2 === 0) {
+//         para[i].style.color = "green"
+//     }
+//     else {
+//         para[i]
+//     }
+// }
+
+// forEach() method
+const ArrOfPara = Array.from(para);
+
+ArrOfPara.forEach((p, index) => {
+    if (index % 2 === 0){
+        p.style.color = "blue"
+    }
+    else{
+        p.style.color = "green";
+    }
+})
+para[para.length-1].style.fontWeight = "bold";
+
+
+// Creating a new element dynamically
+const newPara = document.createElement("p");
+newPara.textContent = "This is a dynamically created paragrpah";
+
+// newPara will not be created in HTMl, until we append it
+// Because, we need to tell DOM, where to keep this element in HTMl document
+
+document.getElementById("content").append(newPara);
+
+
+
+const image = document.createElement("img");
+image.setAttribute("src",
+     "https://tse3.mm.bing.net/th/id/OIP.CGhzovPGwt5YdEau7kekSgHaE8?pid=Api&P=0&h=180");
+image.setAttribute("alt", "cat image");
+document.getElementById("gallery").append(image);
+// snake_case  and camelCase
+// saamp aur oont case
